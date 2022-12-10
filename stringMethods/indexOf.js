b95d3c3624e7d5a7f2647bf3a381e020dd2e8ae9
Neil <dev.haxuan@gmail.com>
2022-12-10 20:58:34 +0700

    string methods

---
+++ stringMethods/indexOf.js
@@ -0,0 +1,7 @@
+/**
+ * methods returns the index of (position of) the 'first' occurrence of a string in a string.
+ *
+ */
+
+let text = "Please locate where 'locate' occurs!";
+console.log(text.indexOf('locate'));
